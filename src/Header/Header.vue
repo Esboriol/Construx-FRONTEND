9d3a1226ae70a3f676bd37cf9bcacd38c77f0a2e
LeoDellAmatrice <Leonardo.dellamatrice@gmail.com>
2025-10-28 17:35:19 -0300

    adição input header

--- src/Header/Header.vue
+++ src/Header/Header.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import NavHeader from "@/Header/nav-header.vue";
+import InputBusca from "@/Header/InputBusca.vue";
 </script>
 
 <template>
   <header class="header">
-    <div class="div-img">
+    <div class="div-logo">
       <img src="/public/favicon.ico" alt="Logo Empresa Construx"/>
+      <h1>Construx</h1>
     </div>
     <div>
-      <h1>
-        Construx
-      </h1>
+      <InputBusca/>
     </div>
     <div class="div-nav">
       <nav-header/>
@@ -27,7 +27,11 @@ header {
   padding: 20px 20px;
 }
 
-.div-img, .div-nav {
+.div-logo {
+  display: flex;
+}
+
+.div-logo, .div-nav {
   flex: 1;
 }
 </style>
---
+++ src/Header/InputBusca.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <input type="text" placeholder="Buscar por produtos" />
+  <button>Buscar</button>
+</template>
+
+<style scoped>
+  input {
+    padding: 5px;
+    border-radius: 5px 0 0 5px;
+    border: 1px solid black;
+  }
+
+  button {
+    padding: 5px;
+    border-radius:  0 5px 5px 0;
+    border: 1px solid black;
+    color: white;
+    background-color: #429aff;
+  }
+</style>
