daf76cfcfab9a36acd2d1f412d22210dc493a157
LeoDellAmatrice <Leonardo.dellamatrice@gmail.com>
2025-10-28 16:35:52 -0300

    create Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup></script>
+<script setup>
+import Header from "@/Header/Header.vue";
+import Footer from "@/Footer/Footer.vue";
+</script>
 
 <template>
+  <Header/>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Footer/>
 </template>
 
 <style scoped></style>
