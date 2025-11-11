f41365fbaf46cbd8da24873fa2dac018a5e8fee9
Esboriol <otavioesboriol@gmail.com>
2025-11-11 15:42:53 -0300

    rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TelaInicial from '../view/TelaInicial.vue'
 import TelaLogin from '../view/TelaLogin.vue'
-import TelaProdutos from '@/view/TelaProdutos.vue'
+import TelaProdutos from '@/view/TelaProduto.vue'
+import TelaProdutos_Categoria from '@/view/TelaProdutos_Categoria.vue'
 
 const routes = [
     {path: '/', name: 'inicial', component: TelaInicial},
     {path: '/login', name: 'login', component: TelaLogin},
-    {path: '/produto', name: 'produto', component: TelaProdutos}
+    {path: '/produto', name: 'produto', component: TelaProdutos},
+    {path: '/produtos', name: 'categorias', component: TelaProdutos_Categoria, props: true}
 ]
 
 const router = createRouter({
